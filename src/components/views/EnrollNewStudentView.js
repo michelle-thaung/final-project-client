58dd672864566c80dcea5d37f5935499cb052dec
Michelle <michellethaung03@gmail.com>
2023-12-18 19:08:48 -0500

    add enroll new student functionaliy

---
+++ src/components/views/EnrollNewStudentView.js
@@ -0,0 +1,98 @@
+/*==================================================
+NewStudentView.js
+
+The Views component is responsible for rendering web page with data provided by the corresponding Container component.
+It constructs a React component to display the new student page.
+================================================== */
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { campus } from '../../store/reducers';
+
+// Create styling for the input form
+const useStyles = makeStyles( () => ({
+  formContainer:{  
+    width: '500px',
+    backgroundColor: '#f0f0f5',
+    borderRadius: '5px',
+    margin: 'auto',
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: 'left',
+    textDecoration: 'none'
+  }, 
+  customizeAppBar:{
+    backgroundColor: '#11153e',
+    shadows: ['none'],
+  },
+  formTitle:{
+    backgroundColor:'#c5c8d6',
+    marginBottom: '15px',
+    textAlign: 'center',
+    borderRadius: '5px 5px 0px 0px',
+    padding: '3px'
+  },
+}));
+
+const EnrollNewStudentView = (props) => {
+  const { campus_id, handleChange, handleSubmit } = props;
+  const classes = useStyles();
+
+  // Render a New Student view with an input form
+  return (
+    <div>
+      <h1>Enroll New Student</h1>
+
+      <div className={classes.root}>
+        <div className={classes.formContainer}>
+          <div className={classes.formTitle}>
+            <Typography style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
+              Add a Student
+            </Typography>
+          </div>
+          <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
+            <label style= {{color:'#11153e', fontWeight: 'bold'}}>First Name: </label>
+            <input type="text" name="firstname" required onChange ={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Last Name: </label>
+            <input type="text" name="lastname" required onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{ color: '#11153e', fontWeight: 'bold' }}>Email: </label>
+            <input type="email" name="email" required onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{ color: '#11153e', fontWeight: 'bold' }}>Image URL: </label>
+            <input type="text" name="imageurl" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <label style={{ color: '#11153e', fontWeight: 'bold'}}>GPA: </label>
+            <input type="number" step="0.1" min="0" max="4" name="gpa" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Campus ID: </label>
+            <input type="text" name="campusId" value={campus_id.campus_id} onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+
+            <Button variant="contained" color="primary" type="submit">
+              Submit
+            </Button>
+            <br/>
+            <br/>
+          </form>
+          </div>
+      </div>
+    </div>    
+  )
+}
+
+export default EnrollNewStudentView;
